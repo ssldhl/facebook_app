0f4d19855fcc29ddd8d683f1fdae11d65e8cd08c
Sushil Dahal <sushilthe@gmail.com>
2015-03-25 10:08:41 +0545

    Finish static pages w/o design

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Lead Chicken Social Spy"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Lead Chicken Social Spy"
   end
 
   test "should get faq" do
     get :faq
     assert_response :success
+    assert_select "title", "FAQ | Lead Chicken Social Spy"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Lead Chicken Social Spy"
   end
 
 end
